2399951b4c55391f73673fc8db55d195daa12e86
ravikumar <dsrkcoin@gmail.com>
2026-04-18 14:47:17 +0530

    login pages added

--- src/components/elements/forms/LoginForm.tsx
+++ src/components/elements/forms/LoginForm.tsx
@@ -4,14 +4,18 @@ import { Fragment } from "react";
 import NextLink from "components/reuseable/links/NextLink";
 
 export default function LoginForm() {
+  const handleLogin = () => {
+    window.location.assign("/api/auth/login");
+  };
+
   return (
     <Fragment>
       <h2 className="mb-3 text-start">Welcome</h2>
       <p className="lead mb-6 text-start">Sign in with Kinde.</p>
 
-      <a href="/api/auth/login" className="btn btn-primary rounded-pill btn-login w-100 mb-2">
+      <button type="button" onClick={handleLogin} className="btn btn-primary rounded-pill btn-login w-100 mb-2">
         Sign In with Kinde
-      </a>
+      </button>
 
       <p className="mb-0">
         Don&apos;t have an account?{" "}
--- src/components/elements/forms/RegisterForm.tsx
+++ src/components/elements/forms/RegisterForm.tsx
@@ -4,11 +4,15 @@ import { Fragment } from "react";
 import NextLink from "components/reuseable/links/NextLink";
 
 export default function RegisterForm() {
+  const handleSignup = () => {
+    window.location.assign("/api/auth/register");
+  };
+
   return (
     <Fragment>
-      <a href="/api/auth/register" className="btn btn-primary rounded-pill btn-login w-100 mb-2">
+      <button type="button" onClick={handleSignup} className="btn btn-primary rounded-pill btn-login w-100 mb-2">
         Sign Up
-      </a>
+      </button>
 
       <p className="mb-0">
         Already have an account? <NextLink href="#" data-bs-toggle="modal" data-bs-target="#modal-signin" className="hover">Sign in</NextLink>
